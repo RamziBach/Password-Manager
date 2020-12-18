bf208e0a10a470c6b073d1ddd3641733255cd345
RamziBach <ramzibach.dev@gmail.com>
2020-12-17 20:27:06 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PasswordManager from './manager/PasswordManager';
+import '../styles/app.css';
+
+const App = () => {
+  return (
+    <>
+      <PasswordManager />
+    </>
+  );
+};
+
+export default App;
